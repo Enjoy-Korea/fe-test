612afc0a9b94bca63959efe7bd7fe22e43ff7880
YumiKim <dwkimym93@gmail.com>
2023-02-24 11:28:27 +0900

    feat: 메인 리스트 sorting 기능 추가

--- src/pages/MainList.jsx
+++ src/pages/MainList.jsx
@@ -1,41 +1,64 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import data from '../data/houses.mock.json';
-import Button from '../components/common/Button';
 import ListItem from '../components/ListItem';
 import Layout from '../components/common/Layout';
+import SelectBox from '../components/common/SelectBox';
+import styled from 'styled-components';
+
+const Ul = styled.ul`
+  margin-top: 1rem;
+`;
 
 const MainList = () => {
-  const [sortingType, setSortingType] = useState('university');
+  const [sortingType, setSortingType] = useState('none');
+  const [sortedData, setSortedData] = useState(data);
 
-  const onClickSortingType = (e) => {
-    const value = e.target.getAttribute('name');
+  useEffect(() => {
+    let newData = [...data];
+
+    newData.sort((a, b) => {
+      return a.id - b.id;
+    });
+
+    if (sortingType === 'university') {
+      newData.sort((a, b) => {
+        if (a.university < b.university) return -1;
+        else if (a.university === b.university) return 0;
+        else return 1;
+      });
+    } else if (sortingType === 'houseType') {
+      newData.sort((a, b) => {
+        if (a.houseType < b.houseType) return -1;
+        else if (a.houseType === b.houseType) return 0;
+        else return 1;
+      });
+    }
+
+    setSortedData(newData);
+  }, [sortingType]);
+
+  const onChangeSortingType = (e) => {
+    const value = e.target.value;
     setSortingType(value);
   };
 
   return (
     <Layout>
       <section>
-        <Button
-          name="university"
-          isActive={sortingType === 'university'}
-          onClick={onClickSortingType}
+        <SelectBox
+          name="sortingType"
+          defaultValue={sortingType}
+          onChange={onChangeSortingType}
         >
-          university
-        </Button>
-        <Button
-          name="houseType"
-          isActive={sortingType === 'houseType'}
-          onClick={onClickSortingType}
-        >
-          houseType
-        </Button>
-        <ul>
-          {data
-            .sort((a, b) => a.id - b.id)
-            .map((item) => (
-              <ListItem key={item.id} item={item} />
-            ))}
-        </ul>
+          <option value="none">정렬기준</option>
+          <option value="university">university</option>
+          <option value="houseType">houseType</option>
+        </SelectBox>
+        <Ul>
+          {sortedData.map((item) => (
+            <ListItem key={item.id} item={item} />
+          ))}
+        </Ul>
       </section>
     </Layout>
   );
