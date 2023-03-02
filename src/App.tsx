9fd233b233b9c623703c7bfe7237442c1a466a01
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2023-03-03 02:12:34 +0900

    add: eslint 및 prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 
 function App() {
-  return <div className="App">Main</div>
+  return <div className="App">Main</div>;
 }
 
-export default App
+export default App;
