7331a9b8f9d20b7e7e3525458900ec9bc995fac5
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2023-03-03 23:47:05 +0900

    feat: University Filter 컴포넌트 추가

--- src/pages/MainListPage/components/UniversitySelect/index.tsx
+++ src/pages/MainListPage/components/UniversitySelect/index.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import FilterSectionLayout from "../FilterSection";
+
+// MEMO: CSS 추가 필요
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  padding: 12px 14px;
+  font-size: 14px;
+  border-radius: 8px;
+
+  /* active */
+  border: 1px solid yellow;
+  background-color: rgba(255, 255, 0, 0.3);
+`;
+
+const RadioBox = styled.input`
+  /* visibility: hidden; */
+`;
+
+const universityTypeList = [
+  { name: "울진대학교", count: 2 },
+  { name: "외국대학교", count: 2 },
+  { name: "서울대학교", count: 2 },
+  { name: "용인대학교", count: 2 }
+];
 
 function UniversitySelect() {
-  return <div>UniversitySelect 택 1</div>;
+  return (
+    <FilterSectionLayout filterWith="University">
+      {universityTypeList.map((university, index) => (
+        <Label htmlFor={university.name} key={`${university.name}-${index}`}>
+          <RadioBox type="radio" name="University" value={university.name} />
+          {university.name} ({university.count})
+        </Label>
+      ))}
+    </FilterSectionLayout>
+  );
 }
 
 export default UniversitySelect;
--- src/pages/MainListPage/index.tsx
+++ src/pages/MainListPage/index.tsx
@@ -3,16 +3,25 @@ import styled from "styled-components";
 
 import HouseCard from "../../components/HouseCard";
 import HouseTypeSelect from "./components/HouseTypeSelect";
+import UniversitySelect from "./components/UniversitySelect";
 
 const Layout = styled.div`
   display: flex;
   width: 100%;
 `;
 
+const FilterSection = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
 function MainListPage() {
   return (
     <Layout>
-      <HouseTypeSelect />
+      <FilterSection>
+        <UniversitySelect />
+        <HouseTypeSelect />
+      </FilterSection>
       <HouseCard />
     </Layout>
   );
