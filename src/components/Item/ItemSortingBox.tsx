6d0bab38e6a75435eff1eb451c305b88fed6a873
JongHoonSon <roka1684@gmail.com>
2023-03-09 00:16:04 +0900

    feat: ItemSortingBox 컴포넌트 구현

--- src/components/Item/ItemSortingBox.tsx
+++ src/components/Item/ItemSortingBox.tsx
@@ -1,18 +1,69 @@
 import React from "react";
 import styled from "styled-components";
+import { Checkbox } from "../common/Checkbox";
+import { VerticalLine } from "./../common/VerticalLine";
 
 export const ItemSortingBox = () => {
-  return <ItemSortingBoxLayout>ItemSortingBox</ItemSortingBoxLayout>;
+  return (
+    <ItemSortingBoxLayout>
+      <SortingOption>
+        <SortingOptionTitleContainer>
+          <SortingOptionTitle>Sort By</SortingOptionTitle>
+        </SortingOptionTitleContainer>
+        <VerticalLine />
+        <CheckboxContainer>
+          <Checkbox title="University" />
+          <Checkbox title="House Type" />
+        </CheckboxContainer>
+      </SortingOption>
+    </ItemSortingBoxLayout>
+  );
 };
 
 const ItemSortingBoxLayout = styled.div`
   width: 100%;
-  height: 100px;
+  min-height: 10%;
 
   padding: 10px;
+  margin: 20px 0px;
 
   border: 3px solid rgb(255, 217, 81);
-  border-radius: 10px;
+  border-radius: 40px;
 
   background-color: #fefefe;
+`;
+
+const SortingOption = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  padding: 10px;
+`;
+
+const SortingOptionTitleContainer = styled.div`
+  min-width: 20%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SortingOptionTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const CheckboxContainer = styled.div`
+  min-width: 20%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
 `;
