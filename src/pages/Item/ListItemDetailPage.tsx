b607fde4c03887b7785f4e8d4e0417f9a455a313
JongHoonSon <roka1684@gmail.com>
2023-03-09 01:57:39 +0900

    feat: houseId path variable 를 읽고 mock data에서 해당하는 house를 찾는 로직 구현

--- src/pages/Item/ListItemDetailPage.tsx
+++ src/pages/Item/ListItemDetailPage.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import houseData from "../../data/houses.mock.json";
 
 export const ListItemDetailPage = () => {
-  return <div>ListItemDetailPage</div>;
+  const { houseId } = useParams();
+  const house = houseData.filter((house) => house.id === Number(houseId))[0];
+
+  return <div>{house.name}</div>;
 };
