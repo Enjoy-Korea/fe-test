00d22c2e7030fc29c415cb50ec6d9137f8ec105f
JongHoonSon <roka1684@gmail.com>
2023-03-08 23:28:47 +0900

    chore: MainListPage 에 사용될 컴포넌트 배치 및 MainListPageLayout 스타일 적용

---
+++ src/components/ItemSortingBox.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ItemSortingBox = () => {
+  return <div>ItemSortingBox</div>;
+};
--- src/pages/MainListPage.tsx
+++ src/pages/MainListPage.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import { ItemList } from "../components/ItemList";
+import { ItemSortingBox } from "../components/ItemSortingBox";
 
 export const MainListPage = () => {
-  return <div>MainListPage</div>;
+  return (
+    <MainListPageLayout>
+      <ItemSortingBox />
+      <ItemList />
+    </MainListPageLayout>
+  );
 };
+
+const MainListPageLayout = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  padding: 50px;
+`;
