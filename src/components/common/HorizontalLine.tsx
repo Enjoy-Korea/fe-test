d6599d3434a8616eb4e020b7d33e87f1f01237b8
JongHoonSon <roka1684@gmail.com>
2023-03-10 00:29:42 +0900

    feat: width prop 추가

--- src/components/common/HorizontalLine.tsx
+++ src/components/common/HorizontalLine.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-export const HorizontalLine = () => {
-  return <HorizontalLineLayout />;
+interface HorizontalLineProps {
+  width?: number;
+}
+
+export const HorizontalLine = ({ width }: HorizontalLineProps) => {
+  return <HorizontalLineLayout width={width ? width : undefined} />;
 };
 
-const HorizontalLineLayout = styled.hr`
-  width: 100%;
+interface HorizontalLineLayoutProps {
+  width?: number;
+}
+
+const HorizontalLineLayout = styled.hr<HorizontalLineLayoutProps>`
+  width: ${(props) => (props.width ? `${props.width}%` : "90%")};
   height: 1px;
 
-  background: rgba(0, 0, 0, 0.1);
+  background: rgba(0, 0, 0, 0.2);
   border: 0;
 `;
